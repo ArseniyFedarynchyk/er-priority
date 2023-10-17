d47a95a8d51d520d5f342d3d10086ccbce960380
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-10-17 21:39:14 +0200

    Create login page component

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'erp-login-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginPageComponent {
+  readonly authForm = this.fb.nonNullable.group({
+    login: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log('Submit was clicked!');
+  }
+}
