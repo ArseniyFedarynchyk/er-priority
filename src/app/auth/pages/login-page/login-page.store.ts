5d3e500ac09841ecba34505e4f617ced6870a5e3
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-10-17 22:04:11 +0200

    Create login-page component store to handle local state

---
+++ src/app/auth/pages/login-page/login-page.store.ts
@@ -0,0 +1,62 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { exhaustMap, tap } from 'rxjs/operators';
+import { AuthService } from '../../services/auth.service';
+
+interface LoginPageState {
+  error: string | null;
+  isPasswordHidden: boolean;
+}
+
+export interface Credentials {
+  login: string;
+  password: string;
+}
+
+@Injectable()
+export class LoginPageStore extends ComponentStore<LoginPageState> {
+  private readonly error$ = this.select(state => state.error);
+  private readonly isPasswordHidden$ = this.select(
+    state => state.isPasswordHidden,
+  );
+
+  readonly vm$ = this.select(
+    {
+      error: this.error$,
+      isPasswordHidden: this.isPasswordHidden$,
+    },
+    { debounce: true },
+  );
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+  ) {
+    super({ error: null, isPasswordHidden: true });
+  }
+
+  togglePasswordVisibility(): void {
+    this.patchState(state => ({ isPasswordHidden: !state.isPasswordHidden }));
+  }
+
+  readonly login = this.effect<Credentials>(credentials$ => {
+    return credentials$.pipe(
+      tap(() => this.patchState({ error: null })),
+      exhaustMap(credentials =>
+        this.authService
+          .authenticate(credentials.login, credentials.password)
+          .pipe(
+            tapResponse({
+              next: () => {
+                this.router.navigate(['/']);
+              },
+              error: (error: HttpErrorResponse) =>
+                this.patchState({ error: error.error.errorMessage }),
+            }),
+          ),
+      ),
+    );
+  });
+}
