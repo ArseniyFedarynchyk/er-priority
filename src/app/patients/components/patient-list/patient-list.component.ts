72d2296a22395e0c4b09cfab0c899bf03881ea2d
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-10-18 13:57:14 +0200

    Patients components and service to retrive data form BE init

---
+++ src/app/patients/components/patient-list/patient-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'erp-patient-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './patient-list.component.html',
+  styleUrls: ['./patient-list.component.scss']
+})
+export class PatientListComponent {
+
+}
---
+++ src/app/patients/components/patient-tile/patient-tile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'erp-patient-tile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './patient-tile.component.html',
+  styleUrls: ['./patient-tile.component.scss']
+})
+export class PatientTileComponent {
+
+}
