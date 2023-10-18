4c6a67930515e147c959c2e7e3fd5989a6ae0194
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-10-18 14:13:45 +0200

    Patient page component init

---
+++ src/app/patients/pages/patient-page/patient-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'erp-patient-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './patient-page.component.html',
+  styleUrls: ['./patient-page.component.scss']
+})
+export class PatientPageComponent {
+
+}
