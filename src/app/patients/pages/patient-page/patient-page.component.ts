2e5540be2192468b7db109856461fc42fc257818
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-10-18 14:36:03 +0200

    Add tooltip to the add patient button

--- src/app/patients/pages/patient-page/patient-page.component.ts
+++ src/app/patients/pages/patient-page/patient-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'erp-patient-page',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './patient-page.component.html',
   styleUrls: ['./patient-page.component.scss'],
 })
