f95563b028de2ce24fdc0e429f85972f6e8e670d
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-10-18 11:32:51 +0200

    Create media querries. Some small changes in styles

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, LayoutModule } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { Observable, map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'erp-toolbar',
@@ -16,8 +18,18 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatIconModule,
     MatButtonModule,
     MatTooltipModule,
+    LayoutModule,
   ],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  constructor(private readonly breakpointObserver: BreakpointObserver) {}
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe('(max-width: 835px)')
+    .pipe(
+      map(result => result.matches),
+      shareReplay(),
+    );
+}
