066c0583cdc972ff64bb5b4004d366266f48f0e2
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-10-21 10:14:16 +0200

    Render patient list component in the patient page component. Change routing to match the page path. Add auth guard to the page.

--- src/app/patients/pages/patient-page/patient-page.component.ts
+++ src/app/patients/pages/patient-page/patient-page.component.ts
@@ -3,12 +3,19 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { PatientListComponent } from '../../components/patient-list/patient-list.component';
 import { PatientPageStore } from './patient-page.store';
 
 @Component({
   selector: 'erp-patient-page',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    PatientListComponent,
+  ],
   templateUrl: './patient-page.component.html',
   styleUrls: ['./patient-page.component.scss'],
   providers: [PatientPageStore],
