6a2f41f7a565e6e2b3a5ff43af575e06d8370433
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-10-22 12:21:48 +0200

    Init add edit dialog component

---
+++ src/app/shared/components/add-edit-dialog/add-edit-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'erp-add-edit-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-edit-dialog.component.html',
+  styleUrls: ['./add-edit-dialog.component.scss']
+})
+export class AddEditDialogComponent {
+
+}
