562ed63f63453a538e8bfdbb01d9475368786200
Arseniy Fedarynchyk <1995arseny@gmail.com>
2023-11-24 09:57:06 +0100

    Init triage dialog

---
+++ src/app/patients/components/patient-triage-dialog/patient-triage-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'erp-patient-triage-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './patient-triage-dialog.component.html',
+  styleUrls: ['./patient-triage-dialog.component.scss']
+})
+export class PatientTriageDialogComponent {
+
+}
