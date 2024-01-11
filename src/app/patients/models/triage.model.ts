e71888b5d66b8e9f032641bdb867f8c82110967b
Arseniy Fedarynchyk <1995arseny@gmail.com>
2024-01-11 10:42:06 +0100

    Create Triage model

---
+++ src/app/patients/models/triage.model.ts
@@ -0,0 +1,16 @@
+export interface Triage {
+  symptoms: string;
+  allergies: string;
+  medicine: string;
+  chronicDiseases: string;
+  circumstances: string;
+  sistolicBloodPressure: string;
+  diastolicBloodPressure: string;
+  puls: string;
+  temperature: string;
+  saturation: string;
+  oxygenTherapy: string;
+  consciousness: string;
+  painIntensity: string;
+  priority: string;
+}
